e26dbfcb6e71160492a5274f9efd5521ee6406a7
Leonardo Peripolli <102085692+leopp18@users.noreply.github.com>
2022-11-11 09:30:24 -0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,10 +1,28 @@
-create database aula
+create database gremio;
 
-create table teste
+select * from teste2
+
+create table teste2
 (
 	id integer primary key identity,
 	nome varchar(50)
 )
+
+create table pessoas2
+(
+	id integer primary key identity,
+	nome varchar(50),
+	idade integer,
+	altura integer
+)
+
+create login usuario2 with password='senha1234';
+create user usuario2 from login usuario2;
+exec sp_addrolemember 'DB_DATAREADER', 'usuario2';
+exec sp_addrolemember 'DB_DATAWRITER', 'usuario2';
+
+select database_id, name, create_date from sys.databases where database_id > 4;
+
 
 begin transaction
 

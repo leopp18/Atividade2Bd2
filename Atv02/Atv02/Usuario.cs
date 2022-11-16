54d7243dd0115b5dbe7a7b27fecd3410a4529043
Leonardo Peripolli <102085692+leopp18@users.noreply.github.com>
2022-11-16 09:14:05 -0300

    atv02

---
+++ Atv02/Atv02/Usuario.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Atv02
+{
+    internal class Usuario
+    {
+        public int id;
+        public string usuario;
+        public string password;
+        public string banco;
+
+        public Usuario(string usuario, string password, string banco)
+        {
+            this.usuario = usuario;
+            this.password = password;
+            this.banco = banco;
+        }
+
+        public void executarTransacao(SqlCommand comando, SqlTransaction transacao)
+        {
+            try
+            {
+                comando.ExecuteNonQuery();
+                transacao.Commit();
+            }
+            catch (Exception ex)
+            {
+                transacao.Rollback();
+            }
+        }
+
+        public bool gravarUsuario()
+        {
+            try
+            {
+                primeiroComando();
+                segundoComando();
+                terceiroComando();
+                return true;
+            }catch(Exception e)
+            {
+                return false;
+            }
+
+            //Banco bd = new Banco();
+            //bd.Db = banco;
+            ////Console.WriteLine(banco + " Printando Banco");
+            //SqlConnection cn;
+
+            //SqlTransaction transacao = cn.BeginTransaction();
+            //SqlCommand comando1 = new SqlCommand();
+            //SqlCommand comando2 = new SqlCommand();
+            //SqlCommand comando3 = new SqlCommand();
+
+            //comando1.Connection = cn;
+            //comando1.Transaction = transacao;
+            //comando1.CommandType = CommandType.Text;
+
+            //comando2.Connection = cn;
+            //comando2.Transaction = transacao;
+            //comando2.CommandType = CommandType.Text;
+
+            //comando3.Connection = cn;
+            //comando3.Transaction = transacao;
+            //comando3.CommandType = CommandType.Text;
+
+            //Message
+            //try
+            //{
+
+            //    //comando1.CommandText = "create login @usuario with password= @password;";
+            //    //comando1.Parameters.Add("@usuario", SqlDbType.VarChar);
+            //    //comando1.Parameters.Add("@password", SqlDbType.VarChar);
+            //    //comando1.Parameters[0].Value = usuario;
+            //    //comando1.Parameters[1].Value = password;
+                
+                
+                
+            //    //comando2.Parameters.Add("@usuario", SqlDbType.VarChar);
+            //    //comando2.Parameters.Add("@usuario2", SqlDbType.VarChar);
+            //    //comando2.Parameters[0].Value = usuario;
+            //    //comando2.Parameters[1].Value = usuario;
+
+            //    Console.WriteLine("Antes do comando 2 com " + usuario);
+            //    executarTransacao(comando2, transacao);
+            //    Console.WriteLine("Depois do comando 2");
+
+            //    comando3.CommandText = "exec sp_addrolemember 'DB_DATAREADER', '" + usuario + "';" + "exec sp_addrolemember 'DB_DATAWRITER', '" + usuario + "';";
+            //    //comando3.Parameters.Add("@usuario", SqlDbType.VarChar);
+            //    //comando3.Parameters[0].Value = usuario;
+            //    executarTransacao(comando3, transacao);
+            //    return true;
+            //}
+            //catch
+            //{
+            //    return false;
+            //}
+            //finally
+            //{
+            //    bd.fecharConexao();
+            //}
+
+
+        }
+        public bool primeiroComando()
+        {
+            Banco bd = new Banco();
+            bd.Db = banco;
+            SqlConnection cn = bd.abrirConexao();
+            SqlTransaction transacao = cn.BeginTransaction();
+
+            SqlCommand comando1 = new SqlCommand();
+
+            comando1.Connection = cn;
+            comando1.Transaction = transacao;
+            comando1.CommandType = CommandType.Text;
+
+            comando1.CommandText = "create login " + usuario + " with password='" + password + "';";
+            try
+            {
+                comando1.ExecuteNonQuery();
+                transacao.Commit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                transacao.Rollback();
+                return false;
+            }
+            finally
+            {
+                bd.fecharConexao();
+            }
+
+        }
+
+        public bool segundoComando()
+        {
+            Banco bd = new Banco();
+            bd.Db = banco;
+            SqlConnection cn = bd.abrirConexao();
+            SqlTransaction transacao = cn.BeginTransaction();
+
+            SqlCommand comando2 = new SqlCommand();
+
+            comando2.Connection = cn;
+            comando2.Transaction = transacao;
+            comando2.CommandType = CommandType.Text;
+
+            comando2.CommandText = "create user " + usuario + " from login " + usuario + ";";
+            try
+            {
+                comando2.ExecuteNonQuery();
+                transacao.Commit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                transacao.Rollback();
+                return false;
+            }
+            finally
+            {
+                bd.fecharConexao();
+            }
+
+        }
+
+        public bool terceiroComando()
+        {
+            Banco bd = new Banco();
+            bd.Db = banco;
+            SqlConnection cn = bd.abrirConexao();
+            SqlTransaction transacao = cn.BeginTransaction();
+
+            SqlCommand comando3 = new SqlCommand();
+
+            comando3.Connection = cn;
+            comando3.Transaction = transacao;
+            comando3.CommandType = CommandType.Text;
+
+            comando3.CommandText = "exec sp_addrolemember 'DB_DATAREADER', '" + usuario + "';" + "exec sp_addrolemember 'DB_DATAWRITER', '" + usuario + "';";
+            try
+            {
+                comando3.ExecuteNonQuery();
+                transacao.Commit();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                transacao.Rollback();
+                return false;
+            }
+            finally
+            {
+                bd.fecharConexao();
+            }
+
+        }
+    }
+}
